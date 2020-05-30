b16d3ab357ab8d2696c085d683a8e463b6eb20b6
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2020-05-30 18:09:25 +0700

    done

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
     name = 'geomepydash',
     package_dir={'': 'C:\/Users/user/documents/githubsite'}, 
-    packages = ['geomepydash'],
-    version = '0.2.3',
+    packages=['gd', 'characters', 'classes'],
+    version = '0.2.4',
     license='MIT',
     description = 'Simple python wrapper for Geometry Dash API and some GD Tools (Originally made by GD Colon)',   # Give a short description about your library
     author = 'vierofernando',
     author_email = 'vierofernando9@gmail.com',
     url = 'https://github.com/vierofernando/geomepydash',
-    download_url = 'https://github.com/vierofernando/geomepydash/archive/0.2.3.tar.gz',
+    download_url = 'https://github.com/vierofernando/geomepydash/archive/0.2.4.tar.gz',
     keywords = ['API', 'WRAPPER', 'GAMES', 'GD'],
     install_requires=[
         'urllib3'
