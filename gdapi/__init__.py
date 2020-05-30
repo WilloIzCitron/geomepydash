03661450a5379696e26fe38ce4b827c8fe6824e6
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2020-05-30 18:00:32 +0700

    added init py

---
+++ gdapi/__init__.py
@@ -0,0 +1,13 @@
+"""
+Geometry Dash API Wrapper
+-----------------
+A basic wrapper for the Discord API.
+:copyright: (c) 2015-2020 Rapptz
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'geomepydash'
+__author__ = 'vierofernando'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020 vierofernando'
+__version__ = '0.2.3'
