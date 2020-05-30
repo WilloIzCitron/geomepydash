bd22c0220b839a0f9a962929dd9f2713fd6dc954
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2020-05-30 17:52:21 +0700

    fixed emptiness

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ setup(
     name = 'geomepydash',
     package_dir={'': 'C:\/Users/user/documents/githubsite'}, 
     packages = ['geomepydash'],
-    version = '0.2.1',
+    version = '0.2.2',
     license='MIT',
     description = 'Simple python wrapper for Geometry Dash API and some GD Tools (Originally made by GD Colon)',   # Give a short description about your library
     author = 'vierofernando',
     author_email = 'vierofernando9@gmail.com',
     url = 'https://github.com/vierofernando/geomepydash',
-    download_url = 'https://github.com/vierofernando/geomepydash/archive/0.2.1.tar.gz',
+    download_url = 'https://github.com/vierofernando/geomepydash/archive/0.2.2.tar.gz',
     keywords = ['API', 'WRAPPER', 'GAMES', 'GD'],
     install_requires=[
         'urllib3'
