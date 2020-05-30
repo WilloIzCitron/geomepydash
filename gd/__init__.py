232976c796c4c39f90ad0755b52ab481e477dfd6
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2020-05-30 18:25:04 +0700

    ANOTHER directory fix

---
+++ gd/__init__.py
@@ -0,0 +1,17 @@
+"""
+geomepydash
+~~~~~~~~~~~~~~~~~~~
+GDBrowser API wrapper and GD Tools generator.
+Originally made by GD Colon. For Geometry dash (game), all rights to RobTop Games.
+(c) 2020 vierofernando
+"""
+
+__title__ = 'gd'
+__author__ = 'vierofernando'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020 vierofernando'
+__version__ = '0.2.4'
+
+from .api import API
+from .characters import Characters
+from .classes import classes
