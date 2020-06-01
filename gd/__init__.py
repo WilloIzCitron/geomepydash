a86392382c06086a890fe3727163aebd6d24b36b
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2020-06-01 17:18:14 +0700

    Update release version 0.3.3

--- gd/__init__.py
+++ gd/__init__.py
@@ -10,7 +10,7 @@ __title__ = 'gd'
 __author__ = 'vierofernando'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2020 vierofernando'
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 
 def classify(obj):
     total = 'class Level:\n\t'
@@ -129,6 +129,43 @@ def fetchLevel(levelid):
         return total
     except AttributeError:
         raise ConnectionRefusedError('Level not found!')
+
+class Comments:
+    def getFromLevel(levelid):
+        if str(levelid).isnumeric()==False:
+            raise ValueError('Parameters must be Level ID. (In an integer form)')
+        try:
+            data = json.loads(fetchapi('https://gdbrowser.com/api/comments/'+str(levelid)+'?top').read())
+            total = []
+            for a in range(0, len(data)):
+                total.append(GDClass(data[a]))
+            return total
+        except:
+            raise ConnectionRefusedError('Invalid Level ID.')
+    
+    def getFromUser(userid):
+        if str(userid).isnumeric()==False:
+            raise ValueError('Parameters (user ID) must be integer value.')
+        try:
+            data = json.loads(fetchapi('https://gdbrowser.com/api/comments/'+str(userid)+'?type=commentHistory').read())
+            total = []
+            for a in range(0, len(data)):
+                total.append(GDClass(data[a]))
+            return total
+        except:
+            raise ConnectionRefusedError('Invalid User ID.')
+    
+    def getProfilePosts(userid):
+        if str(userid).isnumeric()==False:
+            raise ValueError('Parameters (user ID) must be integer value.')
+        try:
+            data = json.loads(fetchapi('https://gdbrowser.com/api/comments/'+str(userid)+'?type=profile').read())
+            total = []
+            for a in range(0, len(data)):
+                total.append(GDClass(data[a]))
+            return total
+        except:
+            raise ConnectionRefusedError('Invalid User ID.')
 
 def levelSearch(query):
     data = json.loads(fetchapi('https://gdbrowser.com/api/search/'+str(query).replace(' ', '%20')).read())
